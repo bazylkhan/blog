a7b61cc790841af99f6eb0b9332f53d5cb3145b1
bazylkhan <bazylkhan.personal@gmail.com>
2025-09-10 01:13:17 +0500

    sidebar, navbar, translate, Button ui, Theme, Theme switcher, Lang switcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,25 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import AppRouter from './providers/router';
+import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 
 const App = () => {
-   const { theme, toggleTheme } = useTheme();
+   const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
+            <Navbar />
+            <Suspense fallback="">
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
             </Suspense>
+            
         </div>
     );
 };
